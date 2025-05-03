3d1bedaeb56ce15e199c2e62917a0b305859e3fc
WeIsWeAre <robokop35@mail.ru>
2025-05-03 23:46:53 +0300

    fix

--- test/ac_noauth/ac_noauth_test.go
+++ test/ac_noauth/ac_noauth_test.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/dmitrovia/passkeeper/internal/client/proc/clientproc/clientpa"
 	"github.com/dmitrovia/passkeeper/internal/client/proc/interactionproc"
@@ -19,7 +20,7 @@ func TestNoAuth(t *testing.T) {
 	t.Helper()
 	t.Parallel()
 
-	// time.Sleep(15 * time.Second)
+	time.Sleep(15 * time.Second)
 
 	clintattr := &clientpa.ClientProcAttr{}
 
